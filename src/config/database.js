ed2905cad6bdcb3b607dd1dbb09d5417acec47ea
Mayank Rawat <70592972+slugneffex@users.noreply.github.com>
2024-10-15 07:50:03 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectdb = async () => {
-  await mongoose.connect("mongodb+srv://slugneffex:slugneffex@cluster0.z8uvi2o.mongodb.net/devTinder");
+  await mongoose.connect("mongodb+srv://user:pass@cluster0.z8uvi2o.mongodb.net/devTinder");
 };
 
 module.exports = connectdb
