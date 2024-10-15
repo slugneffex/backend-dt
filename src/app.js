5a026b8b267e852143499e48b791ed0b621241bc
slugneffex <rawatmayank969@gmail.com>
2024-10-15 07:40:51 +0530

    schema

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const connectdb = require("./config/database");
 
 const app = express();
 
-app.use("/test",(req, res) => {
+app.use("/test", (req, res) => {
   res.send("Hello From the server");
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+
+
+connectdb()
+  .then(() => {
+    console.log("Db Connected successfully");
+    
+    app.listen(3000, () => {
+      console.log("server is running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
